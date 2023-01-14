68feeb56c26ff43754f96da4f67c7580482d7d7b
jiemu <rebianne03@naver.com>
2023-01-14 17:51:41 +0900

    2023.01.14 파이썬 알고리즘 공부 트라이(Trie) - 트라이 구현 - 펠린드롬 페어

---
+++ Trie/implement_trie.py
@@ -0,0 +1,39 @@
+# 트라이 구현(prefix tree)
+# https://leetcode.com/problems/implements-trie-prefix-tree
+
+# 1. 딕셔너리로 구현
+class TrieNode:
+    def __init__(self):
+        self.word = False
+        self.children = {}
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+
+        # 단어가 완성된 트라이라면 true 리턴됨
+        return node.word
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+
+        return True
